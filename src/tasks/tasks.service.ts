9f50c75370e44f3b12a4819e4b3412abe7692fea
memowii <memogl25@gmail.com>
2020-09-17 13:16:30 -0500

    Before Persistence of Getting Tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,28 +13,9 @@ export class TasksService {
     private taskRepository: TaskRepository,
   ) {}
 
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
+  getTasks(filterDto: GetTaskFilterDto) {
 
-  // getTaskWithFilters(filterDto: GetTaskFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-
-  //   let tasks = this.getAllTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter(tasks => tasks.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       task =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
+  }
 
   async getTaskById(id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id);
@@ -64,10 +45,4 @@ export class TasksService {
     await task.save();
     return task;
   }
-
-  // updateTaskStatus(id: string, status: TaskStatus): Task {
-  //   const task = this.getTaskById(id);
-  //   task.status = status;
-  //   return task;
-  // }
 }
