65d9a1b6f8694821a8d948144a09a0e57a637814
memowii <memogl25@gmail.com>
2020-09-21 14:47:24 -0500

    Guarding the Tasks Routes (TasksController)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,10 @@ import {
   Body,
   Controller,
   Post,
-  Req,
-  UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './get-user.decorator';
-import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -30,10 +25,10 @@ export class AuthController {
     return this.authService.signIn(authCredentialsDto);
   }
 
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  // test(@Req() req) {
-  test(@GetUser() user: User) {
-    console.log(user);
-  }
+  // @Post('/test')
+  // @UseGuards(AuthGuard())
+  // // test(@Req() req) {
+  // test(@GetUser() user: User) {
+  //   console.log(user);
+  // }
 }
