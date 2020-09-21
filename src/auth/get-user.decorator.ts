f23d49c3d8b77d9315d685e089b04099d5e6547f
memowii <memogl25@gmail.com>
2020-09-21 14:37:40 -0500

    Custom @GetUser() Decorator

---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,19 @@
+import { createParamDecorator } from '@nestjs/common';
+
+import { User } from './user.entity';
+
+export const GetUser = createParamDecorator(
+  (data, req): User => {
+    return req.user;
+  },
+);
+
+// Solution for nestjs 7
+// import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+// export const GetUser = createParamDecorator(
+//   (data, ctx: ExecutionContext): User => {
+//     const req = ctx.switchToHttp().getRequest();
+//     return req.user;
+//   },
+// );
